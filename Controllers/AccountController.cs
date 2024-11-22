5b38c3abcf5b582257e96ce75dca8ae84e71186d
Olumide Rotimi <rotimiolumide68@gmail.com>
2024-11-22 01:33:25 +0100

    added entitities and AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET: api/<AccountController>
+        [HttpGet("getitems")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("value1");
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
